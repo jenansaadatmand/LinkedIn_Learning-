b59ec0fa3151a247f1fbe5ae4f02941c2dd88588
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-06 22:44:59 -0500

    Update guessing_game.py

--- Fundamentals_of_Programming_Fundamentals/guessing_game.py
+++ Fundamentals_of_Programming_Fundamentals/guessing_game.py
@@ -1,8 +1,8 @@
 # A Guessing game:
 # Ask the user: "What's my favorite food?"
-# If the user enters the name of your favorite food, output:"Yep! So amazing!"
+# If the user enters the name of your favorite food, output: "Yep! So amazing!"
 # If the user does not enter the name of your favorite food, output: "Yuck! That's not it!"
-# Regardless of what the user enters, output:"Thanks for playing!" 
+# Regardless of what the user enters, output: "Thanks for playing!" 
 
 user_input = input("What's my favorite food?")
 
