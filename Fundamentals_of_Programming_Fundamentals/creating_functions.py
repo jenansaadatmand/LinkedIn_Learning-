6658dc26b9aff26a853aaca729a89b4d2ce55b89
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-11 12:08:26 -0500

    Add files via upload

---
+++ Fundamentals_of_Programming_Fundamentals/creating_functions.py
@@ -0,0 +1,9 @@
+# Creating functions:
+
+def say_hello(): # define the function
+    print("Hello friends!")  # function body
+
+
+say_hello()    # need to call the function
+say_hello()
+say_hello()
