4dd0e14138de5c6b2158457fc64ce04fbe8452fb
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-11 00:34:33 -0500

    Add files via upload

---
+++ Fundamentals_of_Programming_Fundamentals/03_07.py
@@ -0,0 +1,14 @@
+print("Challenge 1:")
+
+# A message for the user
+message = "This is going to be tricky ;)"
+Message = "Very tricky!" # Created a new variable with capital "M"
+print(message) # show the message on the screen
+
+# Perform mathematical operations
+result = 2**3
+print("2**3 =", result)
+result = 5 - 3    # updating the result variable with a different value
+#print("5 - 3 =", result)  # This line is commented out and ignored by Python, Python ignores both white spaces and comments 
+
+print("Challenge complete!")
