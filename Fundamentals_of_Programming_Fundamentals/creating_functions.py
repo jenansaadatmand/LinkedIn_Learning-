2f8e05a73e7bc3ddff4f741b5e94cc1a4ca3da89
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-11 12:08:52 -0500

    Update creating_functions.py

--- Fundamentals_of_Programming_Fundamentals/creating_functions.py
+++ Fundamentals_of_Programming_Fundamentals/creating_functions.py
@@ -4,6 +4,6 @@ def say_hello(): # Define the function
     print("Hello friends!")  # function body
 
 
-say_hello()    # need to call the function
+say_hello()    # Need to call the function
 say_hello()
 say_hello()
