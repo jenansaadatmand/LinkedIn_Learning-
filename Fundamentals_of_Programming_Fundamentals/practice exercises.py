aa6c270210e6d7d638b7c5e40cf051c798116f83
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-12 16:10:43 -0500

    Add files via upload

---
+++ Fundamentals_of_Programming_Fundamentals/practice exercises.py
@@ -0,0 +1,26 @@
+# Perform mathematical operations
+result = 2**3
+print("2**3=", result)
+
+print("Practice Exercise 1:")
+
+# A message for the user
+message = "This is going to be tricky. ;)"
+Message = "Very tricky!"
+print(Message)
+
+print("Practice Exercise 2:")
+
+message = "Hope you're having fun!"
+print("The message", message)
+
+# Perform mathematical operations
+# 3**2 is 3^2 (or 3 squared) and 2**3 means 2^3 (or 2 cubed).
+
+answer = 5+2**3
+print("The ansswer is:", answer)
+
+result = 5-3 
+print("5 - 3 =", result)
+
+print("Done!")
