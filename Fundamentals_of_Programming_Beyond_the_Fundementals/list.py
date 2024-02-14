93f0622da72a3f4f356d21ff24b0aa2ab44155b9
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-14 00:17:25 -0500

    Add files via upload

---
+++ Fundamentals_of_Programming_Beyond_the_Fundementals/list.py
@@ -0,0 +1,26 @@
+# Grouping related or unrelated values/things together into a collection or a list
+# city1 = 'Tokyo'
+# city2 = 'Dakar'
+# city3 = 'Mumbai'
+# city4 = 'Buenos Aires'
+
+cities = [
+    'Tokyo',
+    'Dakar', 
+    'Mumbai', 
+    'Buenos Aires',
+    ]
+print(cities)
+print()
+
+print(cities[0])
+print(cities[1])
+print(cities[2])
+print(cities[3])
+print()
+print(cities[-1])
+print(cities[-2])
+print()
+
+
+      
