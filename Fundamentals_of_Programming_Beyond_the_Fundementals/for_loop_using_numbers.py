bfbc9aa8c9ba9696da02b0707255b66c192374e6
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-14 18:35:20 -0500

    Add files via upload

---
+++ Fundamentals_of_Programming_Beyond_the_Fundementals/for_loop_using_numbers.py
@@ -0,0 +1,9 @@
+# Creating a loop using numbers
+# perform a calculation on each of the values
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for number in numbers: 
+    number += 1 # increment by 1
+    print(number)
+    
