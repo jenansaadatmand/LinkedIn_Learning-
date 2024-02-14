81b3354345bcd229cfa5c0a6e9df8df3d0247fb5
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-14 00:18:42 -0500

    Add files via upload

---
+++ Fundamentals_of_Programming_Beyond_the_Fundementals/dictionary.py
@@ -0,0 +1,24 @@
+# A Dictionary stores a collection of related data or information
+
+# California state symbols
+
+# state_bird = 'California quail'
+# state_animal = 'Grizzly bear'
+# state_flower = 'California poppy'
+# state_fruit = 'Avocado'
+
+california_symbols = {
+    "bird":"California quail",
+    "animal":"Grizzly bear",
+    "flower":"California poppy",
+    "fruit":"Avocado", 
+              } # A dictionary with key:value pairs
+
+print(california_symbols)
+
+print(california_symbols["bird"])  # Accessing by key 
+print(california_symbols["fruit"])
+print(california_symbols["flower"])
+
+
+
