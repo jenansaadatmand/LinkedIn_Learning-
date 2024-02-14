4400767ec2b25f72867a0e2e62ad7ea8b26a63a9
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-14 00:19:43 -0500

    Add files via upload

---
+++ Fundamentals_of_Programming_Beyond_the_Fundementals/for_loop.py
@@ -0,0 +1,15 @@
+spices = [
+    'salt',
+    'pepper',
+    'cumin',
+    'turmeric',
+]
+
+
+
+# To print all the items in the list use a for loop
+print(spices)
+print()
+for spice in spices: # Iteration through all the items within the list
+    print(spice)
+print("No more boring omlette") # outside of the for loop
