456c71aacc581e4003843fe8e73960b9cc80a102
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-15 13:57:56 -0500

    Add files via upload

---
+++ Fundamentals_of_Programming_Beyond_the_Fundementals/while_loop.py
@@ -0,0 +1,15 @@
+# Create a loop that counts to 100 by 5
+# i = iterator 
+# Specify end condition, with while a condition that evaluates to True or False
+# After each loop, the statement is checkd,if its False the loop continues. If its True the loop stops and no more loops happen.
+# I want to count to 100, with 100 being the last number printed
+
+i = 5  # Set a counter, start by value 5
+
+print("Count to 100 by fives:")
+
+while i <= 100:  # While loop, while statement
+    print(i) # print the value of i and increase it by 5
+    i += 5  # increment by 10
+print("List complete")
+
