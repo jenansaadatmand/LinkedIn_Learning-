4956b29a591dd7c2b8a1880bacff745c31037816
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-15 14:03:05 -0500

    Add files via upload

---
+++ Fundamentals_of_Programming_Beyond_the_Fundementals/for_loop_challenge.py
@@ -0,0 +1,27 @@
+# Print the names of all the fruits in the Terminal
+# Print each name on a separate line
+# Above the list, print " Our fruit selection:"
+# Use a for loop
+# Outcome: 
+# Our fruit selection
+# apples
+# bananas
+# dragon fruit
+# mangos
+# nectarines
+# pears 
+
+fruits = [
+    'apples',
+    'bananas',
+    'dragon fruit',
+    'mangos',
+    'nectarines',
+    'pears',
+]
+
+print("Our fruit selection:") # Adding a title for the list
+
+for fruit in fruits:
+    print(fruit)
+
