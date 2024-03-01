6d93926593131606499823cb5936b37ee05243ac
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-01 12:26:40 -0500

    Update app04_02.py

--- Fundamentals_of_Programming_Beyond_the_Fundementals/app04_02.py
+++ Fundamentals_of_Programming_Beyond_the_Fundementals/app04_02.py
@@ -6,7 +6,7 @@ first_name_cap = first_name.capitalize()  # Using method capitalize()
 last_name_cap = last_name.capitalize()
 print(first_name_cap)
 print(last_name_cap)
-award_location = note.find('award: ')   # finding a string using .find() method
+award_location = note.find('award: ')   # Finding a string using .find() method
 print(award_location)  # you will get a value of index 0 indicating that the award is at the start of the string
 print()
 
