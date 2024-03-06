9eedd7aa963033d79287919f3583dbdb9892bd39
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-06 10:34:24 -0500

    Update app03_02.py

--- Fundamentals_of_Programming_Beyond_the_Fundementals/app03_02.py
+++ Fundamentals_of_Programming_Beyond_the_Fundementals/app03_02.py
@@ -1,4 +1,4 @@
 # A program that calls the function stored in testmodule.py
 
-import testmodule as tm # assigning the module to a tm shortcut
+import testmodule as tm # Assigning the module to a tm shortcut
 tm.mult(10, 5) # referencing the module.function()
