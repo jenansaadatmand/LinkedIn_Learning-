fd0845a680ca43913a067d7b2ad222c3e29c3672
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-06 10:32:35 -0500

    Update testmodule.py

--- Fundamentals_of_Programming_Beyond_the_Fundementals/testmodule.py
+++ Fundamentals_of_Programming_Beyond_the_Fundementals/testmodule.py
@@ -1,8 +1,9 @@
+# Defining a function that multiplies two numbers together to be used in pp03_2.py exercise
+
 def mult(x, y):  #  A function that multiplies two numbers 
     print(f'{x} * {y} = {x * y}') # String formatting with f-string and {} pace holders (formatted string literal)
     
 
 
 mult(2, 4) # to call the function 
-
 
